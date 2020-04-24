15970c6a4b628925e4ce14dd3508d7b640c92ca0
GarnerJames <gmacvey@gmail.com>
2020-04-25 11:27:13 +1200

    Ladder
    Adding ladder and teleport

---
+++ Pilot - Prototype/Assets/Scripts/LadderClimb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderClimb : MonoBehaviour
+{
+
+    public GameObject player;
+    public Transform locationTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if ((other.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.RightControl))
+        {
+            player.transform.position = locationTo.transform.position;
+        }
+    }
+}
